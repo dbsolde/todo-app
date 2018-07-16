b37c62d76f8d5e99d90b88a2255d5273180d2d9a
Dennis S <dbmsolde@gmail.com>
2018-07-16 20:06:52 +0800

    clean up

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import FontAwesome from 'react-fontawesome'
 import './style.css';
 import PlusImage from '../../asset/images/plus-icon.png'
 import TextInput from '../TextInput'
